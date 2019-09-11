371da518c170ed4debb9d2e89cbf381156a38e71
Holger Büch (CI/DAD2.13) <holger.buech@de.bosch.com>
2019-09-11 16:51:00 +0200

    Fix build on windows

--- build.py
+++ build.py
@@ -3,8 +3,6 @@ import os
 
 # TODO: Switch based on platform
 
-os.chdir("./normcap")
-
 PyInstaller.__main__.run(
     [
         "--name=normcap",
@@ -20,7 +18,6 @@ PyInstaller.__main__.run(
         "--hidden-import=PIL", 
         "--hidden-import=PIL._imagingtk",
         "--hidden-import=PIL._tkinter_finder",
-        "--hidden-import=utils.log_dataclass",
         os.path.join("normcap","normcap.py"),
     ]
 )
