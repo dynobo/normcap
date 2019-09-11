994368ffc8de6cc9a4df64a887ce88ee1df5794d
dynobo <dynobo@mailbox.org>
2019-09-11 18:42:13 +0200

    use os path

--- build.py
+++ build.py
@@ -8,16 +8,16 @@ PyInstaller.__main__.run(
         "--name=normcap",
         "--clean",
         "--noconfirm",
-        "-OO",  # Bytecode Optimization
+        # "-OO",  # Bytecode Optimization
         # "--onefile",
         "--onedir",
-        #"--windowed",
-        "-D", # debug
-        # "--icon=%s" % os.path.join("ressource", "normcap.ico"),
-        "--paths=./normcap",
-        "--hidden-import=PIL", 
+        # "--windowed",
+        "-D",  # debug
+        # f"--icon={os.path.join('ressource', 'normcap.ico')}",
+        f"--paths={os.path.join('.', 'normcap')}",
+        "--hidden-import=PIL",
         "--hidden-import=PIL._imagingtk",
         "--hidden-import=PIL._tkinter_finder",
-        os.path.join("normcap","normcap.py"),
+        os.path.join("normcap", "normcap.py"),
     ]
 )
