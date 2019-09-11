aa56d8e809576ab815f4da96ffff5e999c9fb5bd
dynobo <dynobo@mailbox.org>
2019-09-11 07:34:51 +0200

    Change cwd; Add bytecode optimization

--- build.py
+++ build.py
@@ -3,12 +3,15 @@ import os
 
 # TODO: Switch based on platform
 
+os.chdir("./normcap")
+
 PyInstaller.__main__.run(
     [
         "--name=%s" % "normcap",
         "--clean",
         "--noconfirm",
-        #"--onefile",
+        "-OO",  # Bytecode Optimization
+        # "--onefile",
         "--onedir",
         "--windowed",
         # "--icon=%s" % os.path.join("ressource", "normcap.ico"),
