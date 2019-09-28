2e8ecb1dff52786384f43d0e9a1177e3f0fc2966
dynobo <dynobo@mailbox.org>
2019-09-28 20:08:13 +0200

    Add test for argparser

--- normcap/test/test_normcap.py
+++ normcap/test/test_normcap.py
@@ -1,10 +1,16 @@
 """Unit tests for main normcap logic."""
 
 # Default
-import logging
+import logging  #
+
+# Extra
+import pytest
 
 # Own
 from normcap import normcap
+
+# PyLint can't handle fixtures correctly. Ignore.
+# pylint: disable=redefined-outer-name
 
 # For Docstrings in pytests, see:
 # https://stackoverflow.com/questions/28898919/use-docstrings-to-list-tests-in-py-test
@@ -21,6 +27,43 @@ def test_init_logging_returns_logger():
     assert isinstance(logger, logging.Logger)
 
 
-# def test_parse_cli_args():
-#    result = normcap.parse_cli_args()
-#    assert isinstance(result, dict)
+# TESTING create_argparser()
+
+
+@pytest.fixture(scope="session")
+def argparser_defaults():
+    """Create argparser and provide its default values."""
+    argparser = normcap.create_argparser()
+    return vars(argparser.parse_args([]))
+
+
+def test_argparser_defaults_complete(argparser_defaults):
+    """Check if all default options are available."""
+    args_keys = set(argparser_defaults.keys())
+    expected_options = set(["verbose", "mode", "lang", "color", "path"])
+    assert args_keys == expected_options
+
+
+def test_argparser_default_verbose(argparser_defaults):
+    """Check verbose (for loglevel)."""
+    assert argparser_defaults["verbose"] is False
+
+
+def test_argparser_default_mode(argparser_defaults):
+    """Check default capture mode."""
+    assert argparser_defaults["mode"] == "trigger"
+
+
+def test_argparser_default_lang(argparser_defaults):
+    """Check OCR language."""
+    assert argparser_defaults["lang"] == "eng"
+
+
+def test_argparser_default_color(argparser_defaults):
+    """Check accent color."""
+    assert argparser_defaults["color"] == "#FF0000"
+
+
+def test_argparser_default_path(argparser_defaults):
+    """Check path to store images."""
+    assert argparser_defaults["path"] is None
