f166437b880df0ec748e327998ac551cd8ea9639
dynobo <dynobo@mailbox.org>
2019-11-03 16:43:35 +0100

    Initial version of setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="normcap",
+    version="0.1.0-alpha",
+    description="Intelligent screencapture tool to capture information instead of images.",
+    keywords="screenshot ocr capture",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dynobo/normcap",
+    author="dynobo",
+    author_email="dynobo@mailbox.org",
+    license="GPLv3",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Multimedia :: Graphics :: Capture :: Screen Capture",
+        "Topic :: Utilities",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+    ],
+    packages=["normcap"],
+    include_package_data=False,
+    python_requires=">=3.7.0",
+    install_requires=["mss", "Pillow", "tesserocr", "pyperclip", "python-Levenshtein"],
+    entry_points={"console_scripts": ["normcap=normcap.normcap:main",]},
+)
