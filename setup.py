3864227caccdf26754017a9b039c9e5c339fe1cd
dynobo <dynobo@mailbox.org>
2019-11-03 22:52:51 +0100

    Add platform dependend tesserocr

--- setup.py
+++ setup.py
@@ -1,11 +1,36 @@
+# Standard
 import pathlib
-from setuptools import setup
+import platform
+from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
+
+# Requirements,
+INSTALL_REQUIRES = ["mss", "Pillow", "tesserocr", "pyperclip", "python-Levenshtein"]
+
+# If Platform is window, exchange tesserocr with pre compiled wheel from
+# https://github.com/simonflueckiger/tesserocr-windows_build/releases
+TESSEROCR_PYPI = "tesserocr"
+TESSEROCR_WIN32 = (
+    "https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
+    "tesserocr-v2.4.0-tesseract-4.0.0/tesserocr-2.4.0-cp37-cp37m-win32.whl"
+)
+TESSEROCR_WIN64 = (
+    "https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
+    "tesserocr-v2.4.0-tesseract-4.0.0/tesserocr-2.4.0-cp37-cp37m-win_amd64.whl"
+)
+
+if platform.system().lower() == "windows":
+    if platform.machine().endswith("64"):
+        INSTALL_REQUIRES.remove(TESSEROCR_PYPI)
+        INSTALL_REQUIRES.append(TESSEROCR_WIN64)
+    if platform.machine().endswith("86"):
+        INSTALL_REQUIRES.remove(TESSEROCR_PYPI)
+        INSTALL_REQUIRES.append(TESSEROCR_WIN32)
 
 # This call to setup() does all the work
 setup(
@@ -31,9 +56,9 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
     ],
-    packages=["normcap"],
+    packages=find_packages(exclude=("tests",)),
     include_package_data=False,
     python_requires=">=3.7.0",
-    install_requires=["mss", "Pillow", "tesserocr", "pyperclip", "python-Levenshtein"],
+    install_requires=INSTALL_REQUIRES,
     entry_points={"console_scripts": ["normcap=normcap.normcap:main",]},
 )
