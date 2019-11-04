cd0d15f9de3bb7eaf3cdda2e40bf5a7847aaf6a5
dynobo <dynobo@users.noreply.github.com>
2019-11-04 09:31:34 +0100

    Fix urls

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ class install_tesserocr(install):
     # https://github.com/simonflueckiger/tesserocr-windows_build/releases
     TESSEROCR = { "pypi": "tesserocr",
                   "win32" : (
-                                "tesserocr@git+https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
+                                "https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
                                 "tesserocr-v2.4.0-tesseract-4.0.0/tesserocr-2.4.0-cp37-cp37m-win32.whl"
                             ),
                   "win64": (
-                                "tesserocr@git+https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
+                                "https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
                                 "tesserocr-v2.4.0-tesseract-4.0.0/tesserocr-2.4.0-cp37-cp37m-win_amd64.whl"
                             )
                 }
