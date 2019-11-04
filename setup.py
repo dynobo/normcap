08eee698ead2e4cee2e34cc6fb11e22d0f7d2616
dynobo <dynobo@users.noreply.github.com>
2019-11-04 08:59:28 +0100

    Try using postinstall for tesserocr

--- setup.py
+++ setup.py
@@ -2,35 +2,37 @@
 import pathlib
 import platform
 from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
 
+class install_tesserocr(_install):
+  def run(self):
+    _install.do_egg_install(self)
+    
+    # If Platform is window, exchange tesserocr with pre compiled wheel from
+    # https://github.com/simonflueckiger/tesserocr-windows_build/releases
+    TESSEROCR = { "pypi": "tesserocr",
+                  "win32" : (
+                                "tesserocr@git+https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
+                                "tesserocr-v2.4.0-tesseract-4.0.0/tesserocr-2.4.0-cp37-cp37m-win32.whl"
+                            )
+                  "win64": (
+                                "tesserocr@git+https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
+                                "tesserocr-v2.4.0-tesseract-4.0.0/tesserocr-2.4.0-cp37-cp37m-win_amd64.whl"
+                            )
+    tess_version = "pypi"
+    if platform.system().lower() == "windows":
+        if platform.machine().endswith("64"):
+            tess_version = "win64"
+        if platform.machine().endswith("86"):
+            tess_version = "win32"
+ 
+    pip.main(['install', TESSEROCR[tess_version]])
+    
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
-
-# Requirements,
-INSTALL_REQUIRES = ["mss", "Pillow", "tesserocr", "pyperclip", "python-Levenshtein"]
-
-# If Platform is window, exchange tesserocr with pre compiled wheel from
-# https://github.com/simonflueckiger/tesserocr-windows_build/releases
-TESSEROCR_PYPI = "tesserocr"
-TESSEROCR_WIN32 = (
-    "tesserocr@git+https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
-    "tesserocr-v2.4.0-tesseract-4.0.0/tesserocr-2.4.0-cp37-cp37m-win32.whl"
-)
-TESSEROCR_WIN64 = (
-    "tesserocr@git+https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/"
-    "tesserocr-v2.4.0-tesseract-4.0.0/tesserocr-2.4.0-cp37-cp37m-win_amd64.whl"
-)
-
-if platform.system().lower() == "windows":
-    if platform.machine().endswith("64"):
-        INSTALL_REQUIRES.remove(TESSEROCR_PYPI)
-        INSTALL_REQUIRES.append(TESSEROCR_WIN64)
-    if platform.machine().endswith("86"):
-        INSTALL_REQUIRES.remove(TESSEROCR_PYPI)
-        INSTALL_REQUIRES.append(TESSEROCR_WIN32)
 
 # This call to setup() does all the work
 setup(
@@ -59,6 +61,7 @@ setup(
     packages=find_packages(exclude=("tests",)),
     include_package_data=False,
     python_requires=">=3.7.0",
-    install_requires=INSTALL_REQUIRES,
+    install_requires=["mss", "Pillow", "pyperclip", "python-Levenshtein"],
     entry_points={"console_scripts": ["normcap=normcap.normcap:main",]},
+    cmdclass={'install': install_tesserocr}
 )
