827b82ad840624e6009ca0a992959e8aa25ed1d7
dynobo <dynobo@mailbox.org>
2019-12-08 21:23:38 +0100

    adjust hidden imports

--- build.py
+++ build.py
@@ -11,8 +11,11 @@ PyInstaller.__main__.run(
         "--windowed",
         # "-d=all",
         f"--icon={os.path.join('ressource', 'normcap.ico')}",
-        "--hidden-import=PIL",
-        "--hidden-import=PIL._imagingtk",
+        "--hidden-import=tkinter",
+        "--hidden-import=mss",
+        "--hidden-import=Pillow",
+        "--hidden-import=pyperclip",
+        # "--hidden-import=PIL._imagingtk",
         "--hidden-import=PIL._tkinter_finder",
         os.path.join("normcap", "normcap.py"),
     ]
