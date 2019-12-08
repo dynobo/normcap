95efe318a23de3bb52369f1af58ad3fbd3213fa9
dynobo <dynobo@mailbox.org>
2019-12-08 18:57:10 +0100

    remove exception, as not needed

--- tests/test_capture_handler.py
+++ tests/test_capture_handler.py
@@ -1,10 +1,6 @@
 """Unit tests for screenshot capture handler."""
-# Default
-import os
-
 # Extra
 from PIL import Image
-from mss.exception import ScreenShotError
 
 # Own
 from normcap.common.data_model import NormcapData
@@ -15,15 +11,7 @@ def test_capture_handler():
     """Test if screenshots are added to data object."""
     data = NormcapData()
     capture = CaptureHandler()
-    try:
-        result = capture.handle(data)
-        assert (len(result.shots) > 0) and (
-            isinstance(result.shots[0]["image"], Image.Image)
-        )
-    except ScreenShotError:
-        # We expect this error if no Xserver is available:
-        x_available = False
-        if "DISPLAY" in os.environ:
-            print(f"\n\nWARNING: Display set to '{os.environ['DISPLAY']}'\n\n")
-            x_available = True
-        assert x_available is False
+    result = capture.handle(data)
+    assert (len(result.shots) > 0) and (
+        isinstance(result.shots[0]["image"], Image.Image)
+    )
