266697cc00509c5e05147f71c0f621b38710c522
dynobo <dynobo@mailbox.org>
2019-12-08 17:40:45 +0100

    Handle expetion in case xserver not available

--- tests/test_capture_handler.py
+++ tests/test_capture_handler.py
@@ -2,6 +2,7 @@
 
 # Extra
 from PIL import Image
+from mss.exception import ScreenShotError
 
 # Own
 from normcap.common.data_model import NormcapData
@@ -11,8 +12,18 @@ from normcap.handlers.capture_handler import CaptureHandler
 def test_capture_handler():
     """Test if screenshots are added to data object."""
     data = NormcapData()
-    capture = CaptureHandler()
-    result = capture.handle(data)
-    assert (len(result.shots) > 0) and (
-        isinstance(result.shots[0]["image"], Image.Image)
-    )
+    try:
+        capture = CaptureHandler()
+        result = capture.handle(data)
+        assert (len(result.shots) > 0) and (
+            isinstance(result.shots[0]["image"], Image.Image)
+        )
+    except ScreenShotError:
+        # We get this error if e.g. no Xserver is available.
+        # Fallback to test_mode, as needed by github actions
+        capture = CaptureHandler()
+        data.test_mode = True
+        result = capture.handle(data)
+        assert (len(result.shots) > 0) and (
+            isinstance(result.shots[0]["image"], Image.Image)
+        )
