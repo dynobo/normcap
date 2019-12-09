484b414262c037591a2f0f3c76bb97c0ef6d07bc
dynobo <dynobo@mailbox.org>
2019-12-09 12:42:49 +0100

    Add OS switch

--- build.py
+++ build.py
@@ -1,26 +1,24 @@
 import PyInstaller.__main__
 import os
 
-PyInstaller.__main__.run(
-    [
-        "--name=NormCap",
-        "--clean",
-        "--noconfirm",
-        # "--onefile",
-        "--onedir",
-        "--windowed",
-        # "--debug=all",
-        "--log-level=DEBUG",
-        f"--icon={os.path.join('ressource', 'normcap.ico')}",
-        f"--paths={os.path.join('.venv', 'lib', 'python3.7', 'site-packages')}",
-        # "--hidden-import=tkinter",
-        # "--hidden-import=mss",
-        # "--hidden-import=pyperclip",
-        # "--hidden-import=PIL",
-        "--hidden-import=PIL._imagingtk",
-        "--hidden-import=PIL._tkinter_finder",
-        "--add-data=/etc/fonts:fonts",
-        "--runtime-hook=rthook.py",
-        os.path.join("normcap", "__main__.py"),
-    ]
-)
+ARGS = [
+    "--name=NormCap",
+    "--clean",
+    "--noconfirm",
+    # "--onefile",
+    "--onedir",
+    "--windowed",
+    # "--debug=all",
+    "--log-level=DEBUG",
+    f"--icon={os.path.join('ressource', 'normcap.ico')}",
+    f"--paths={os.path.join('.venv', 'lib', 'python3.7', 'site-packages')}",
+    "--hidden-import=PIL._imagingtk",
+    "--hidden-import=PIL._tkinter_finder",
+]
+
+if False:
+    ARGS.extend(["--add-data=/etc/fonts:fonts", "--runtime-hook=rthook.py"])
+
+ARGS.append(os.path.join("normcap", "__main__.py"))
+
+PyInstaller.__main__.run(ARGS)
