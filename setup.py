24ecfa39c6034f72b326cb9f5459bbfcd9f5b076
dynobo <dynobo@mailbox.org>
2019-12-13 20:24:36 +0100

    Bump version: 0.0.10 → 0.0.11

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="normcap",
-    version="0.0.10",
+    version="0.0.11",
     description="Intelligent screencapture tool to capture information instead of images.",
     keywords="screenshot ocr capture",
     long_description=README,
