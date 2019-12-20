c28dccae5389b46655b34f1fad447d8fc421c465
dynobo <dynobo@mailbox.org>
2019-12-21 00:05:40 +0100

    Add padding to images (should help ocr)

--- normcap/handlers/enhance_img_handler.py
+++ normcap/handlers/enhance_img_handler.py
@@ -24,7 +24,7 @@ class EnhanceImgHandler(AbstractHandler):
         # Currently, the image is only enlarged
         if request.image:
             request.image = self._enlarge_dpi(request.image)
-        # request.image = self._grayscale(request.image)
+            request.image = self._add_padding(request.image)
         # request.image = self._strech_contrast(request.image)
 
         if self._next_handler:
@@ -39,6 +39,20 @@ class EnhanceImgHandler(AbstractHandler):
     def _strech_contrast(self, img: Image.Image) -> Image.Image:
         img = ImageOps.autocontrast(img)
         return img
+
+    def _add_padding(self, img: Image.Image) -> Image.Image:
+        bg_col = self._most_frequent_colour(img)
+        img = ImageOps.expand(img, border=20, fill=bg_col)
+        return img
+
+    def _most_frequent_colour(self, img):
+        w, h = img.size
+        pixels = img.getcolors(w * h)
+        most_frequent_pixel = pixels[0]
+        for count, colour in pixels:
+            if count > most_frequent_pixel[0]:
+                most_frequent_pixel = (count, colour)
+        return most_frequent_pixel[1]
 
     def _enlarge_dpi(self, img: Image.Image) -> Image.Image:
         """Resize image to get equivalent of 300dpi.
