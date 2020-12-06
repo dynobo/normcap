373205a0b23875b4ea55c8fab411684e31e75c99
dynobo <dynobo@mailbox.org>
2020-12-06 17:48:47 +0100

    Separate assets and (deployed) ressources

---
+++ assets/icon/create_icon.md
@@ -0,0 +1,6 @@
+# Create icon from SVG source
+
+1. Export SVG to PNG (512x512) with Inkscape
+2. Upload to <https://convertico.com/>
+3. Select sizes: 256, 128, 64, 48, 32, 24
+4. Convert and download
---
+++ assets/screencast/produce_error_msg.sh
@@ -0,0 +1,1 @@
+(sleep .2; wmctrl -r SomeError -e 0,3100,-1,-1,-1) | zenity --error --text="An annoying error occurred\! You should search the internet for... \n\nCritical Error X-742891-DE-35321 \n\n...but this message is not selectable, hehe! \n\n \t\t\t\t\t\t\t-- Unknown Dev" --title="SomeError" --width=300
