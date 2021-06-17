6e5d6dd680c223bc7d7a8b6dd92013d232136bb1
dynobo <dynobo@mailbox.org>
2021-06-17 17:33:05 +0200

    Bump version: 0.1.12 → 0.1.13

--- tests/test_normcap.py
+++ tests/test_normcap.py
@@ -22,7 +22,7 @@ from .images.test_images import TEST_IMAGES
 
 def test_version():
     """Are we testing right version?"""
-    assert normcap.__version__ == "0.1.12"
+    assert normcap.__version__ == "0.1.13"
 
 
 # TESTING client_code()
