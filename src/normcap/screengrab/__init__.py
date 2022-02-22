f3ffe9bc2e6fcb4c67fba3ab979c52a4603b486d
dynobo <dynobo@mailbox.org>
2022-02-22 21:54:40 +0100

    Use dbus shell for screenshot only for old gnome

--- src/normcap/screengrab/__init__.py
+++ src/normcap/screengrab/__init__.py
@@ -6,11 +6,12 @@ if not display_manager_is_wayland():
     from normcap.screengrab import qt
 
     grab_screens = qt.grab_screens
-elif gnome_shell_version() and gnome_shell_version() >= version.parse("41"):
-    from normcap.screengrab import dbus_portal
-
-    grab_screens = dbus_portal.grab_screens
-else:
+elif gnome_shell_version() and gnome_shell_version() < version.parse("41"):
     from normcap.screengrab import dbus_shell
 
     grab_screens = dbus_shell.grab_screens
+
+else:
+    from normcap.screengrab import dbus_portal
+
+    grab_screens = dbus_portal.grab_screens
