3e48ca66265fb59aceb8724e9b594eb2d4df5869
dynobo <dynobo@mailbox.org>
2022-12-24 14:59:15 +0100

    feat: use jeepney for legacy dbus portal calls on older pyside versions

---
+++ src/tests/tests_screengrab/test_init.py
@@ -0,0 +1,9 @@
+import PySide6
+import pytest
+from normcap.screengrab import _is_pyside6_64plus
+
+
+@pytest.mark.parametrize("version,result", (("6.2.1", False), ("6.4.2", True)))
+def test_is_pyside6_64plus(monkeypatch, version, result):
+    monkeypatch.setattr(PySide6, "__version__", version)
+    assert _is_pyside6_64plus() is result
