67d7a3d05f83ed6c830f9fb1bea8378f6384e685
dynobo <dynobo@mailbox.org>
2023-01-08 14:51:14 +0100

    feat: return download failed reason

--- src/tests/tests_gui/test_downloader.py
+++ src/tests/tests_gui/test_downloader.py
@@ -16,19 +16,25 @@ def test_downloader_retrieves_website(qtbot):
 
 
 def test_downloader_handles_not_existing_url(caplog, qtbot):
+    wrong_url = "https://not_existing_url.normcap"
+
     downloader = Downloader()
     # Do not trigger download finished signal on error
     with qtbot.waitSignal(
         downloader.com.on_download_finished, raising=False, timeout=4000
     ) as result:
-        downloader.get("https://not_existing_url.normcap")
+        downloader.get(wrong_url)
 
     assert not result.args
     assert not result.signal_triggered
     assert "ERROR" in caplog.text
+    assert wrong_url in caplog.text
+    assert "Exception" in caplog.text
 
     # Do trigger download failed signal
     with qtbot.waitSignal(downloader.com.on_download_failed, timeout=4000) as result:
-        downloader.get("https://not_existing_url.normcap")
+        downloader.get(wrong_url)
 
     assert result.signal_triggered
+    assert wrong_url in result.args[0]
+    assert "Exception" in result.args[0]
