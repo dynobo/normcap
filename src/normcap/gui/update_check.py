23f57940413e80436776f29ce32833a54a5f3193
dynobo <dynobo@mailbox.org>
2023-01-10 21:18:12 +0100

    refactor: remove normcap.version from gui and delete module

--- src/normcap/gui/update_check.py
+++ src/normcap/gui/update_check.py
@@ -8,7 +8,6 @@ from normcap import __version__
 from normcap.gui.constants import URLS
 from normcap.gui.downloader import Downloader
 from normcap.gui.utils import get_icon, set_cursor
-from normcap.version import Version
 
 logger = logging.getLogger(__name__)
 
@@ -16,8 +15,7 @@ logger = logging.getLogger(__name__)
 class Communicate(QtCore.QObject):
     """TrayMenus' communication bus."""
 
-    on_version_parsed = QtCore.Signal(str)
-    on_new_version_found = QtCore.Signal(str)
+    on_version_checked = QtCore.Signal(str)
     on_click_get_new_version = QtCore.Signal(str)
 
 
@@ -30,8 +28,6 @@ class UpdateChecker(QtCore.QObject):
         self.com = Communicate()
         self.downloader = Downloader()
         self.downloader.com.on_download_finished.connect(self._parse_version)
-        self.com.on_version_parsed.connect(self._check_if_version_is_new)
-        self.com.on_new_version_found.connect(self._show_update_message)
         self.message_box = self._create_message_box()
 
     def _parse_version(self, data: bytes) -> None:
@@ -51,7 +47,12 @@ class UpdateChecker(QtCore.QObject):
             logger.exception("Parsing response of update check failed: %s", e)
 
         if newest_version:
-            self.com.on_version_parsed.emit(newest_version)
+            logger.debug(
+                "Newest version: %s (installed: %s)", newest_version, __version__
+            )
+            self.com.on_version_checked.emit(newest_version)
+            if self._is_new_version(current=__version__, other=newest_version):
+                self._show_update_message(new_version=newest_version)
         else:
             logger.error("Couldn't detect remote version. Update check won't work!")
 
@@ -70,19 +71,51 @@ class UpdateChecker(QtCore.QObject):
         message_box.setDefaultButton(QtWidgets.QMessageBox.StandardButton.Ok)
         return message_box
 
-    def _check_if_version_is_new(self, newest_version: str) -> None:
-        """Show dialog informing about available update."""
-        if newest_version:
-            logger.debug(
-                "Newest version: %s (installed: %s)", newest_version, __version__
-            )
-            if Version(newest_version) > Version(__version__):
-                self.com.on_new_version_found.emit(newest_version)
+    @staticmethod
+    def _is_new_version(current: str, other: str) -> bool:
+        """Compare version strings.
 
-    def _show_update_message(self, new_version: Version) -> None:
+        As the versions compare are within the scope of this project, a simple parsing
+        logic will do. It is based on the assumption that semver and only the suffixes
+        "alpha" and "beta" are used.
+
+        NOTE: This is not very robust, so do not use elsewhere! But the standard
+        solution packaging.version is not used here on purpose to avoid that dependency.
+        That reduces importtime and package size.
+        """
+        current_tuple = tuple([c.zfill(8) for c in re.split(r"\.|\-", current)])
+        other_tuple = tuple([c.zfill(8) for c in re.split(r"\.|\-", other)])
+
+        # Compare just the major.minor.patch for the clear cases
+        if other_tuple[:3] > current_tuple[:3]:
+            return True
+
+        if other_tuple[:3] < current_tuple[:3]:
+            return False
+
+        if len(other_tuple) == 3 and len(current_tuple) == 3:
+            return False
+
+        # We have one or more pre-release suffixes to handle.
+
+        # Check if only one version has suffix, which also is a clear case:
+        if len(other_tuple) != len(current_tuple):
+            return len(other_tuple) < len(current_tuple)
+
+        # Check if the suffix (alpha, beta) are different by comparing first letters
+        if current_tuple[3][0] != other_tuple[3][0]:
+            return other_tuple[3][0] > current_tuple[3][0]
+
+        # Suffixes are the same, compare numbers
+        current_suffix_digits = re.sub(r"[^0-9]", "", current_tuple[3])
+        other_suffix_digits = re.sub(r"[^0-9]", "", other_tuple[3])
+        return int(other_suffix_digits) > int(current_suffix_digits)
+
+    def _show_update_message(self, new_version: str) -> None:
         """Show dialog informing about available update."""
         text = f"<b>NormCap v{new_version} is available.</b> (You have v{__version__})"
         if self.packaged:
+            # TODO: Move longer strings to contants module
             info_text = (
                 "You can download the new version for your operating system from "
                 "GitHub.\n\n"
