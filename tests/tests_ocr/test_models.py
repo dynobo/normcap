aa5e632ebb5f2f8092e50cd65174faa6f80013a4
dynobo <dynobo@mailbox.org>
2023-01-20 23:25:42 +0100

    test: add test for TessArgs

--- tests/tests_ocr/test_models.py
+++ tests/tests_ocr/test_models.py
@@ -1,3 +1,6 @@
+from normcap.ocr.models import TessArgs
+
+
 def test_ocr_result(ocr_result):
     """Check if calulated properties are working correctely."""
     assert isinstance(ocr_result.text, str)
@@ -21,3 +24,25 @@ def test_ocr_result(ocr_result):
     assert ocr_result.best_scored_magic is None
     ocr_result.magic_scores = {"email": 1.0, "url": 0.7, "paragraph": 0}
     assert ocr_result.best_scored_magic == "email"
+
+
+def test_tess_args_jpn():
+    tess_args = TessArgs(
+        path="./tessdata", lang="jpn", oem=111, psm=222, version="4.1.1"
+    )
+    config = tess_args.to_config_str()
+    assert '--tessdata-dir "./tessdata"' in config
+    assert "-c preserve_interword_spaces=1" in config
+    assert "--oem 111" in config
+    assert "--psm 222" in config
+    assert tess_args.is_language_without_spaces()
+
+
+def test_tess_args_eng():
+    tess_args = TessArgs(path=None, lang="eng", oem=111, psm=222, version="4.1.1")
+    config = tess_args.to_config_str()
+    assert "--oem 111" in config
+    assert "--psm 222" in config
+    assert "--tessdata-dir" not in config
+    assert "-c preserve_interword_spaces=1" not in config
+    assert not tess_args.is_language_without_spaces()
