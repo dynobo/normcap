547d804f685cc995e2241d88c54c13787d6c9473
dynobo <dynobo@mailbox.org>
2023-01-22 18:23:34 +0100

    fix: use png icons in resources instead of svg

--- tests/tests_gui/test_resources.py
+++ tests/tests_gui/test_resources.py
@@ -9,7 +9,7 @@ from normcap.gui import resources
 
 def test_resources_complete(qtbot):
     icons_path = Path(importlib_resources.files("normcap.resources")) / "icons"
-    icons_files = [f.name for f in icons_path.glob("*.svg")]
+    icon_files = [f.name for f in icons_path.glob("*.png")]
 
     assert resources.qt_resource_data
     assert resources.qt_resource_name
@@ -19,8 +19,26 @@ def test_resources_complete(qtbot):
     root = tree.getroot()
     icons_qrc = [el.text for el in root.find("qresource").findall("file")]
 
-    assert set(icons_qrc) == set(icons_files)
+    assert set(icons_qrc) == set(icon_files)
 
     for icon in icons_qrc:
         stem = icon.split(".")[0]
         assert QtGui.QIcon(f":{stem}").availableSizes(), stem
+
+
+def test_resources_png_and_svg_exist_for_all_icons(qtbot):
+    icons_path = Path(importlib_resources.files("normcap.resources")) / "icons"
+    icon_names_svg = [f.stem for f in icons_path.glob("*.svg")]
+    icon_names_png = [f.stem for f in icons_path.glob("*.png")]
+
+    assert set(icon_names_png) == set(icon_names_svg)
+
+
+def test_all_icon_can_be_loaded(qapp):
+    icons_path = Path(importlib_resources.files("normcap.resources")) / "icons"
+    icon_names = [f.stem for f in icons_path.glob("*.png")]
+
+    for icon in icon_names:
+        qicon = QtGui.QIcon(f":{icon}")
+        assert qicon
+        assert len(qicon.availableSizes()) >= 1
