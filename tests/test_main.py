9ec69cd0b6b04b72faf4b1d64ff10bc1622fa96b
dynobo <dynobo@mailbox.org>
2023-02-26 19:16:08 +0100

    test: add test for main entry point

---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+from importlib.machinery import SourceFileLoader
+from importlib.util import module_from_spec, spec_from_loader
+from pathlib import Path
+
+from normcap import app
+
+
+def test_main_starts_normcap(monkeypatch):
+    called = False
+
+    def mocked_app_main():
+        nonlocal called
+        called = True
+
+    monkeypatch.setattr(app, "main", mocked_app_main)
+    main_module = str(
+        (Path(__file__).parent.parent / "normcap" / "__main__.py").absolute()
+    )
+    loader = SourceFileLoader("__main__", main_module)
+    loader.exec_module(module_from_spec(spec_from_loader(loader.name, loader)))
+    assert called
