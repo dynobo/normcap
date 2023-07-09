1338467d2a873386513014d21e2194d57a53223e
dynobo <dynobo@mailbox.org>
2023-07-09 13:38:44 +0200

    Revert "chore: use appropriate timeouts when testing downloader"
    This reverts commit 59aced3e02965d68f65468e3c9ea95f7d607a56e.

--- tests/tests_gui/test_tray.py
+++ tests/tests_gui/test_tray.py
@@ -22,7 +22,7 @@ def test_debug_language_manager_is_deactivated(qapp):
 )
 def test_sanitize_active_language(qapp, monkeypatch, active, available, sanitized):
     monkeypatch.setattr(ocr.tesseract, "get_languages", lambda **kwargs: available)
-    settings = Settings("TEST_normcap", "settings")
+    settings = Settings(organization="normcap_TEST")
     try:
         settings.setValue("language", active)
         tray_cls = tray.SystemTray
