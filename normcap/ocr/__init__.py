b20b1dec5703fb900471e851241b760b251a58d3
dynobo <dynobo@mailbox.org>
2023-07-24 18:08:45 +0200

    chore: add todo and module description

--- normcap/ocr/__init__.py
+++ normcap/ocr/__init__.py
@@ -1,3 +1,5 @@
 from . import enhance, models, recognize, tesseract
 
 __all__ = ["recognize", "tesseract", "models", "enhance"]
+
+# TODO: Restructure the package to use relative imports for future carve-out
