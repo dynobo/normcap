58d6494d70c20d857bbf73e3f5d490cf6b509b35
dynobo <dynobo@mailbox.org>
2023-09-21 20:55:35 +0200

    test: enable clipboard test on macOS and Windows

--- tests/tests_gui/test_clipboard.py
+++ tests/tests_gui/test_clipboard.py
@@ -1,12 +1,6 @@
-import sys
-
-import pytest
-
 from normcap import clipboard
 
 
-# TODO: Check if this test doesn't run on other platforms
-@pytest.mark.skipif(sys.platform == "linux", reason="Linux specific test?")
 def test_copy_to_clipboard(qapp):
     text = "To be copied to system clipboard"
     _copy_to_clipboard = clipboard.get_copy_func()
