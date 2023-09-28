115dd655b037cb22850eb49578edb9267364fd6d
dynobo <dynobo@mailbox.org>
2023-09-28 22:16:09 +0200

    chore(ocr): remove inverting images as tesseract already does this

--- tests/tests_ocr/test_enhance.py
+++ tests/tests_ocr/test_enhance.py
@@ -1,7 +1,6 @@
 from collections import Counter
 from pathlib import Path
 
-import pytest
 from PySide6 import QtCore, QtGui
 
 from normcap.ocr import enhance
@@ -48,36 +47,11 @@ def test_resize_image():
     assert img.height() * factor == img_result.height()
 
 
-@pytest.mark.parametrize("pixel", [(0, 0), (99, 99)])
-def test_invert_image(pixel):
-    img = QtGui.QImage(Path(__file__).parent / "testimages" / "color.png")
-    img_result = enhance.invert_image(img.copy())
-
-    points = [(x, y) for x in range(img.width()) for y in range(img.height())]
-    img_orig_pixels = enhance._get_pixels(img, points=points)
-    img_result_pixels = enhance._get_pixels(img_result, points=points)
-
-    pixel_pairs = list(zip(img_orig_pixels, img_result_pixels))
-    pixel_wise_sum = [list(map(sum, zip(*p))) for p in pixel_pairs]
-    assert all(s == [255, 255, 255] for s in pixel_wise_sum)
-
-
-def test_is_dark():
-    img = QtGui.QImage(Path(__file__).parent / "testimages" / "dark.png")
-    assert enhance.is_dark(img)
-    img = QtGui.QImage(Path(__file__).parent / "testimages" / "light.png")
-    assert not enhance.is_dark(img)
-
-
 def test_preprocess():
     img = QtGui.QImage(Path(__file__).parent / "testimages" / "dark.png")
     factor = 2
     padding = 10
     img_result = enhance.preprocess(img.copy(), resize_factor=factor, padding=padding)
     assert img.width() * factor + padding * 2 == img_result.width()
-    assert enhance._get_pixels(image=img_result, points=[(0, 0)])[0] == (255, 255, 255)
-    assert enhance._get_pixels(image=img_result, points=[(99, 49)])[0] == (
-        255,
-        255,
-        255,
-    )
+    assert enhance._get_pixels(image=img_result, points=[(0, 0)])[0] == (0, 0, 0)
+    assert enhance._get_pixels(image=img_result, points=[(99, 49)])[0] == (0, 0, 0)
