aacb86dc13324395242310f1606da8c0979a9977
dynobo <dynobo@mailbox.org>
2023-11-04 00:46:28 +0100

    test(integration): add paragraph test cases

--- tests/integration/testcases/data.py
+++ tests/integration/testcases/data.py
@@ -9,6 +9,8 @@ class TestCase:
     image_path: Path
     expected_ocr_text: str
     expected_ocr_magics: list[str]
+    expected_similarity: float = 0.98
+    skip: bool = False
 
     _image: QtGui.QImage | None = None
 
@@ -110,6 +112,67 @@ testcases: tuple[TestCase, ...] = (
             "there. Astonishing!\n"
             "How can that be? Aren't the contents of a hotel room drawer inaccessible "
             "if you haven't rented the room?"
+        ),
+        expected_ocr_magics=["ParagraphMagic"],
+    ),
+    TestCase(
+        # https://www.gutenberg.org/cache/epub/1321/pg1321-images.html
+        image_path=image_dir / "08_paragraphs.png",
+        expected_ocr_text=(
+            "Chapter 17\n"
+            "The being finished speaking and fixed his looks upon me in the "
+            "expectation of a reply. But I was bewildered, perplexed, and unable to "
+            "arrange my ideas sufficiently to understand the full extent of his "
+            "proposition. He continued,\n"
+            "“You must create a female for me with whom I can live in the interchange "
+            "of those sympathies necessary for my being. This you alone can do, and I "
+            "demand it of you as a right which you must not refuse to concede.”\n"
+            "The latter part of his tale had kindled anew in me the anger that had "
+            "died away while he narrated his peaceful life among the cottagers, and as "
+            "he said this I could no longer suppress the rage that burned within me."
+        ),
+        expected_ocr_magics=["ParagraphMagic"],
+        expected_similarity=1,
+    ),
+    TestCase(
+        image_path=image_dir / "09_two_columns.png",
+        expected_ocr_text=(
+            "Optical character recognition or optical character reader (OCR) is the "
+            "electronic or mechanical conversion of images of typed, handwritten or "
+            "printed text into machine-encoded text, whether from a scanned document, "
+            "a photo of a document, a scene photo (for example the text on signs and "
+            "billboards in a landscape photo) or from subtitle text superimposed on an "
+            "image (for example: from a television broadcast).\n"
+            "Widely used as a form of data entry from printed paper data records — "
+            "whether passport documents, invoices, bank statements, computerized "
+            "receipts, business cards, mail, printed data, or any suitable "
+            "documentation — it is a common method of digitizing printed texts so that "
+            "they can be electronically edited, searched, stored more compactly, "
+            "displayed online, and used in machine processes such as cognitive "
+            "computing, machine translation, (extracted) text-to-speech, key data and "
+            "text mining. OCR is a field of research in pattern recognition, "
+            "artificial intelligence and computer vision.\n"
+            "Early versions needed to be trained with images of each character, and "
+            "worked on one font at a time. Advanced systems capable of producing a "
+            "high degree of accuracy for most fonts are now common, and with support "
+            "for a variety of image file format inputs. Some systems are capable "
+            "of reproducing formatted output that closely approximates the original "
+            "page including images, columns, and other non-textual components."
+        ),
+        expected_ocr_magics=["ParagraphMagic"],
+    ),
+    TestCase(
+        image_path=image_dir / "10_font_sizes.png",
+        expected_ocr_text=(
+            "Arial, 8 pt - You only live once, but if you do it right, once is enough."
+            "\n"
+            "Arial, 11 pt - You only live once, but if you do it right, once is enough."
+            "\n"
+            "Arial, 14 pt - You only live once, but if you do it right, once is enough."
+            "\n"
+            "Arial, 16 pt - You only live once, but if you do it right, once is enough."
+            "\n"
+            "Arial, 22 pt - You only live once, but if you do it right, once is enough."
         ),
         expected_ocr_magics=["ParagraphMagic"],
     ),
