de32475aefd4ac5a95926bcd5dbb8d5f202b4c65
dynobo <dynobo@mailbox.org>
2023-12-04 14:55:52 +0100

    build: fix openssl download url

--- bundle/platforms/windows_briefcase.py
+++ bundle/platforms/windows_briefcase.py
@@ -22,8 +22,9 @@ class WindowsBriefcase(BuilderBase):
     def _download_openssl(self) -> None:
         """Download openssl needed for QNetwork https connections."""
         # For mirrors see: https://wiki.openssl.org/index.php/Binaries
-        # OPENSSL_URL = "http://mirror.firedaemon.com/OpenSSL/openssl-1.1.1q.zip"
-        openssl_url = "http://wiki.overbyte.eu/arch/openssl-1.1.1q-win64.zip"
+        # openssl_url = "http://mirror.firedaemon.com/OpenSSL/openssl-1.1.1q.zip"
+        # openssl_url = "http://wiki.overbyte.eu/arch/openssl-1.1.1q-win64.zip"
+        openssl_url = "https://indy.fulgan.com/SSL/openssl-1.0.2u-x64_86-win64.zip"
         target_path = self.PROJECT_PATH / "normcap" / "resources" / "openssl"
         target_path.mkdir(exist_ok=True)
         zip_path = self.BUILD_PATH / "openssl.zip"
