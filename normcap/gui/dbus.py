e592d1320f55b0063c2cbf9e49d09b1d4586bffe
dynobo <dynobo@mailbox.org>
2024-01-07 23:00:11 +0100

    refactor(gui): remove qtdbus versions of window positioning

--- normcap/gui/dbus.py
+++ normcap/gui/dbus.py
@@ -1,22 +1,13 @@
 import json
 import logging
 import os
-import sys
 import tempfile
 import traceback
-from typing import Any, cast
 
 from jeepney.io.blocking import Proxy, open_dbus_connection
 from jeepney.wrappers import Message, MessageGenerator, new_method_call
 
 from normcap.gui.models import Rect
-
-try:
-    from PySide6 import QtDBus
-
-except ImportError:
-    QtDBus = cast(Any, None)
-
 
 logger = logging.getLogger(__name__)
 
@@ -119,66 +110,6 @@ def move_window_via_gnome_shell_eval(title_id: str, position: Rect) -> bool:
         return True
 
 
-def move_window_via_gnome_shell_eval_qtdbus(title_id: str, position: Rect) -> bool:
-    """Move currently active window to a certain position.
-
-    TODO: Deprecated. Remove, once jeepney is confirmed working!
-
-    This is a workaround for not being able to reposition windows on wayland.
-    It only works on Gnome Shell.
-
-    Args:
-        title_id: Window title (has to be unique)
-        position: Target geometry
-
-    Returns:
-        If call was successful
-    """
-    if sys.platform != "linux" or not QtDBus:
-        raise TypeError("QtDBus should only be called on Linux systems!")
-
-    logger.debug(
-        "Moving window '%s' to %s via org.gnome.Shell.Eval", title_id, position
-    )
-    js_code = f"""
-    const GLib = imports.gi.GLib;
-    global.get_window_actors().forEach(function (w) {{
-        var mw = w.meta_window;
-        if (mw.get_title() == "{title_id}") {{
-            mw.move_resize_frame(
-                0,
-                {position.left},
-                {position.top},
-                {position.width},
-                {position.height}
-            );
-        }}
-    }});
-    """
-    item = "org.gnome.Shell"
-    interface = "org.gnome.Shell"
-    path = "/org/gnome/Shell"
-
-    bus = QtDBus.QDBusConnection.sessionBus()
-    if not bus.isConnected():
-        logger.error("Not connected to dbus!")
-
-    shell_interface = QtDBus.QDBusInterface(item, path, interface, bus)
-    if not shell_interface.isValid():
-        logger.warning("Invalid dbus interface on Gnome")
-        return False
-
-    response = shell_interface.call("Eval", js_code)
-    success = response.arguments()[0] if response.arguments() else False
-    if response.errorName() or not success:
-        logger.error("Failed to move Window via org.gnome.Shell.Eval!")
-        logger.error("Error: %s", response.errorMessage())
-        logger.error("Response arguments: %s", response.arguments())
-        return False
-
-    return True
-
-
 def move_window_via_kde_kwin_scripting(title_id: str, position: Rect) -> bool:
     """Move currently active window to a certain position.
 
@@ -227,69 +158,6 @@ def move_window_via_kde_kwin_scripting(title_id: str, position: Rect) -> bool:
             return False
         else:
             return True
-
-
-def move_window_via_kde_kwin_scripting_qtdbus(title_id: str, position: Rect) -> bool:
-    """Move currently active window to a certain position.
-
-    TODO: Deprecated. Remove, once jeepney is confirmed working!
-
-    This is a workaround for not being able to reposition windows on wayland.
-    It only works on KDE.
-
-    Args:
-        title_id: Window title (has to be unique)
-        position: Target geometry
-
-    Returns:
-        If call was successful
-    """
-    if sys.platform != "linux" or not QtDBus:
-        raise TypeError("QtDBus should only be called on Linux systems!")
-
-    logger.debug(
-        "Moving window '%s' to %s via org.kde.kwin.Scripting", title_id, position
-    )
-    js_code = f"""
-    const clients = workspace.clientList();
-    for (var i = 0; i < clients.length; i++) {{
-        if (clients[i].caption() == "{title_id}" ) {{
-            clients[i].geometry = {{
-                "x": {position.left},
-                "y": {position.top},
-                "width": {position.width},
-                "height": {position.height}
-            }};
-        }}
-    }}
-    """
-    with tempfile.NamedTemporaryFile(delete=True, suffix=".js") as script_file:
-        script_file.write(js_code.encode())
-
-        bus = QtDBus.QDBusConnection.sessionBus()
-        if not bus.isConnected():
-            logger.error("Not connected to dbus!")
-            return False
-
-        item = "org.kde.KWin"
-        interface = "org.kde.kwin.Scripting"
-        path = "/Scripting"
-        shell_interface = QtDBus.QDBusInterface(item, path, interface, bus)
-
-        # FIXME: shell_interface is not valid on latest KDE in Fedora 36.
-        if not shell_interface.isValid():
-            logger.warning("Invalid dbus interface on KDE")
-            return False
-
-        x = shell_interface.call("loadScript", script_file.name)
-        y = shell_interface.call("start")
-        logger.debug("KWin loadScript response: %s", x.arguments())
-        logger.debug("KWin start response: %s", y.arguments())
-        if x.errorName() or y.errorName():
-            logger.error("Failed to move Window via org.kde.kwin.Scripting!")
-            logger.error(x.errorMessage(), y.errorMessage())
-
-    return True
 
 
 def move_windows_via_window_calls_extension(title_id: str, position: Rect) -> bool:
