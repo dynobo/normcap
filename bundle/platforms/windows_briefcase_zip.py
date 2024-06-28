1f96f9a9c10f11610b61fcfbce10da6265b9b165
mfz <>
2024-06-28 14:21:08 +0800

    feat: support portable mode on Windows

--- bundle/platforms/windows_briefcase_zip.py
+++ bundle/platforms/windows_briefcase_zip.py
@@ -1,5 +1,7 @@
 """Run adjustments while packaging with briefcase during CI/CD."""
 
+from pathlib import Path
+
 from platforms.windows_briefcase  import WindowsBriefcase
 
 
@@ -15,4 +17,15 @@ class WindowsBriefcaseZip(WindowsBriefcase):
         self._patch_main_cpp()
         self.run(cmd="briefcase build windows VisualStudio", cwd=self.PROJECT_PATH)
         self._patch_windows_installer()
+        portable_flag_file = (
+            self.PROJECT_PATH
+            / "build"
+            / "normcap"
+            / "windows"
+            / "visualstudio"
+            / "x64"
+            / "Release"
+            / "portable"
+        )
+        Path(portable_flag_file).touch()
         self.run(cmd="briefcase package windows VisualStudio -p zip", cwd=self.PROJECT_PATH)
