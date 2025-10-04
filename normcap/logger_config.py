5aa62e49e644daee4753079b31d33685ab8e21e2
dynobo <dynobo@mailbox.org>
2025-10-05 01:19:06 +0200

    feat(logging): improve format for better clickable pathnames

--- normcap/logger_config.py
+++ normcap/logger_config.py
@@ -15,6 +15,16 @@ logger = logging.getLogger(__name__)
 _ISSUES_URLS = "https://github.com/dynobo/normcap/issues/new"
 
 
+class ShortenPathnameFilter(logging.Filter):
+    """Filter to shorten log record pathname from absolute to relative."""
+
+    _root_path_length = len(str(Path(__file__).parent.resolve())) + 1
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        record.pathname = record.pathname[self._root_path_length :]
+        return True
+
+
 def _is_wayland_display_manager() -> bool:
     xdg_session_type = os.environ.get("XDG_SESSION_TYPE", "").lower()
     has_wayland_display_env = bool(os.environ.get("WAYLAND_DISPLAY", ""))
@@ -23,12 +33,14 @@ def _is_wayland_display_manager() -> bool:
 
 def _init_logger(log_level: str = "WARNING", log_file: Path | None = None) -> None:
     """Initializes a logger with a specified log level."""
-    log_format = "%(asctime)s - %(levelname)-7s - %(name)s:%(lineno)d - %(message)s"
+    log_format = "%(asctime)s - %(levelname)-7s - %(pathname)s:%(lineno)d - %(message)s"
     datefmt = "%H:%M:%S"
 
     handlers: list[logging.Handler] = []
 
-    handlers.append(logging.StreamHandler())
+    stream_handler = logging.StreamHandler()
+    stream_handler.addFilter(ShortenPathnameFilter())
+    handlers.append(stream_handler)
 
     # On Windows, stream handler output is not visible. Therefore, we log to file on
     # desktop, if the level was set to more detailed than Warning (the default).
@@ -41,7 +53,9 @@ def _init_logger(log_level: str = "WARNING", log_file: Path | None = None) -> No
 
     if log_file:
         log_file.parent.mkdir(parents=True, exist_ok=True)
-        handlers.append(logging.FileHandler(log_file, mode="w"))
+        file_handler = logging.FileHandler(log_file, mode="w")
+        file_handler.addFilter(ShortenPathnameFilter())
+        handlers.append(file_handler)
 
     logging.basicConfig(
         format=log_format,
