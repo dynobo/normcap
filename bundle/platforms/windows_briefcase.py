38b52696a7190c21f6d49418292533dba1968c17
dynobo <dynobo@mailbox.org>
2026-07-10 13:15:25 +0200

    fix: windows packaging fails after briefcase update

--- bundle/platforms/windows_briefcase.py
+++ bundle/platforms/windows_briefcase.py
@@ -1,9 +1,7 @@
 """Run adjustments while packaging with briefcase during CI/CD."""
 
-import shutil
 import urllib.request
 import zipfile
-from xml.etree import ElementTree as ET
 
 from retry import retry
 
@@ -33,84 +31,6 @@ class WindowsBriefcase(BuilderBase):
 
         zip_path.unlink()
 
-    def _patch_windows_installer(self) -> None:
-        """Customize wix-installer."""
-        wxs_file = (
-            self.PROJECT_PATH
-            / "build"
-            / "normcap"
-            / "windows"
-            / "visualstudio"
-            / "normcap.wxs"
-        )
-
-        ns = "{http://wixtoolset.org/schemas/v4/wxs}"
-        ET.register_namespace("", ns.strip("{}"))
-
-        tree = ET.parse(wxs_file)  # noqa: S314
-        root = tree.getroot()
-        package = root.find(f"{ns}Package")
-        if package is None:
-            raise ValueError(
-                "Package section not found!\n"
-                f"File: {wxs_file.resolve()}, Content:\n{wxs_file.read_text()}"
-            )
-
-        # Copy installer images
-        left = "normcap_install_bg.bmp"
-        top = "normcap_install_top.bmp"
-
-        for image in (left, top):
-            original = self.IMG_PATH / image
-            target = (
-                self.PROJECT_PATH
-                / "build"
-                / "normcap"
-                / "windows"
-                / "visualstudio"
-                / image
-            )
-            shutil.copy(original, target)
-
-        # Set installer images
-        ET.SubElement(
-            package, f"{ns}WixVariable", {"Id": "WixUIDialogBmp", "Value": f"{left}"}
-        )
-        ET.SubElement(
-            package, f"{ns}WixVariable", {"Id": "WixUIBannerBmp", "Value": f"{top}"}
-        )
-
-        # Cleanup tessdata folder on uninstall
-        ET.SubElement(
-            package,
-            "CustomAction",
-            {
-                "Id": "CleanupOrphanedFiles",
-                "Directory": "TARGETDIR",
-                "ExeCommand": 'cmd /C rmdir /s /q "%localappdata%\\normcap"',
-                "Execute": "deferred",
-                "Return": "ignore",
-                "HideTarget": "no",
-                "Impersonate": "no",
-            },
-        )
-        sequence = package.find(f"{ns}InstallExecuteSequence")
-        if sequence is None:
-            sequence = ET.SubElement(package, f"{ns}InstallExecuteSequence")
-        ET.SubElement(
-            sequence,
-            "Custom",
-            {
-                "Action": "CleanupOrphanedFiles",
-                "Before": "RemoveFiles",
-                "Condition": 'REMOVE = "ALL" AND NOT UPGRADINGPRODUCTCODE',
-            },
-        )
-
-        with wxs_file.open("wb") as fh:
-            ET.indent(tree=tree)
-            tree.write(fh, encoding="utf-8", xml_declaration=False)
-
     def bundle_tesseract(self) -> None:
         """Download tesseract binaries including dependencies into resource path."""
         bundle_tesseract_windows_ub_mannheim(self)
@@ -118,7 +38,6 @@ class WindowsBriefcase(BuilderBase):
     def run_framework(self) -> None:
         self.run(cmd="briefcase create windows VisualStudio", cwd=self.PROJECT_PATH)
         self.run(cmd="briefcase build windows VisualStudio", cwd=self.PROJECT_PATH)
-        self._patch_windows_installer()
         self.run(cmd="briefcase package windows VisualStudio", cwd=self.PROJECT_PATH)
 
     def install_system_deps(self) -> None: ...
---
+++ bundle/scripts/uninstall_tessdata.bat
@@ -0,0 +1,1 @@
+rmdir /s /q "%localappdata%\normcap"
